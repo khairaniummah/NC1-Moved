b29d78b201ecd01b24f1811529738f1d89003413
Khairani Ummah <khairani.u@gmail.com>
2019-05-20 18:15:13 +0700

    tuing tuing is moving

--- NC1-Moved/ViewController.swift
+++ NC1-Moved/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bloob: UIImageView!
+    
+    var bloobImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bloobImages = createImageArray(total: 17, imagePrefix: "a")
+        animate(imageView: bloob, images: bloobImages)
         // Do any additional setup after loading the view.
+        
+        
     }
-
+    
+    func createImageArray(total: Int, imagePrefix: String) -> [UIImage] {
+        var imageArray: [UIImage] = []
+        
+        for imageCount in 1..<total {
+            let imageName = "\(imagePrefix)\(imageCount).png"
+            let image = UIImage(named: imageName)!
+            imageArray.append(image)
+        }
+        
+        return imageArray
+    }
+    
+    func animate(imageView: UIImageView, images: [UIImage]) {
+        imageView.animationImages = images
+        imageView.animationDuration = 2.0
+        imageView.animationRepeatCount = 10
+        imageView.startAnimating()
+    }
 
 }
 
