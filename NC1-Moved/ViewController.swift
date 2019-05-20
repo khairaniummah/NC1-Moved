d8fd6bd245472e04e0d8fdae99aba961facc63c2
Khairani Ummah <khairani.u@gmail.com>
2019-05-20 17:18:02 +0700

    Initial Commit

---
+++ NC1-Moved/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1-Moved
+//
+//  Created by Khairani Ummah on 20/05/19.
+//  Copyright © 2019 Khairani Ummah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
